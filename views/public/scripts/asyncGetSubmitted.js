62e4873a2a2bce0ae9c1ca536ea3bd2b822a5ceb
Brandon Stillwell <brstillwell@csumb.edu>
2017-11-28 00:49:36 +0000

    bug fixes

--- views/public/scripts/asyncGetSubmitted.js
+++ views/public/scripts/asyncGetSubmitted.js
@@ -8,21 +8,18 @@ try {
     $.when(
       
       await $.getJSON('http://' + ipAddress + '/api/BarrierSubs/barrierSubGeoJSON', function (data) {
-      //await $.getJSON('http://snap-restoration-brstillwell.c9users.io/api/BarrierSubs/barrierSubGeoJSON', function (data) {
         barriers = data[0].row_to_json;
         if (barriers.features != null)
           overallCount += barriers.features.length;
       }),
         
       await $.getJSON('http://' + ipAddress + '/api/DistPointSubs/distPointSubGeoJSON', function (data) {
-      //await $.getJSON('http://snap-restoration-brstillwell.c9users.io/api/DistPointSubs/distPointSubGeoJSON', function (data) {
         distPoints = data[0].row_to_json;
         if (distPoints.features != null)
           overallCount += distPoints.features.length;
       }),
     
       await $.getJSON('http://' + ipAddress + '/api/DistLineSubs/distLineSubGeoJSON', function (data) {
-      //$.getJSON('http://snap-restoration-brstillwell.c9users.io/api/DistLineSubs/distLineSubGeoJSON', function (data) {
         distLines = data[0].row_to_json;
         if (distLines.features != null)
           overallCount += distLines.features.length;
@@ -30,19 +27,16 @@ try {
       }),
     
       await $.getJSON('http://' + ipAddress + '/api/DistPolygonSubs/distPolySubGeoJSON', function (data) {
-      //await $.getJSON('http://snap-restoration-brstillwell.c9users.io/api/DistPolygonSubs/distPolySubGeoJSON', function (data) {
         distPolys = data[0].row_to_json;
         if (distPolys.features != null)
           overallCount += distPolys.features.length;
       }),
     
       /*//$.getJSON('http://' + ipAddress + '/api/DistPolyCentroids/distPolyCentGeoJSON', function (data) {
-      $.getJSON('http://snap-restoration-brstillwell.c9users.io/api/DistPolyCentroids/distPolyCentGeoJSON', function (data) {
         // some code
       }),*/
     
       await $.getJSON('http://' + ipAddress + '/api/RestoPointSubs/restoPointSubGeoJSON', function (data) {
-      //await $.getJSON('http://snap-restoration-brstillwell.c9users.io/api/RestoPointSubs/restoPointSubGeoJSON', function (data) {
         //console.log(data);
         restoPoints = data[0].row_to_json;
         if (restoPoints.features != null)
@@ -50,14 +44,12 @@ try {
       }),
     
       await $.getJSON('http://' + ipAddress + '/api/RestoLineSubs/restoLineSubGeoJSON', function (data) {
-      //await $.getJSON('http://snap-restoration-brstillwell.c9users.io/api/RestoLineSubs/restoLineSubGeoJSON', function (data) {
         restoLines = data[0].row_to_json;
         if (restoLines.features != null)
           overallCount += restoLines.features.length;
       }),
     
       await $.getJSON('http://' + ipAddress + '/api/RestoPolygonSubs/restoPolySubGeoJSON', function (data) {
-      //await $.getJSON('http://snap-restoration-brstillwell.c9users.io/api/RestoPolygonSubs/restoPolySubGeoJSON', function (data) {
         restoPolys = data[0].row_to_json;
         if (restoPolys.features != null)
           overallCount += restoPolys.features.length;
