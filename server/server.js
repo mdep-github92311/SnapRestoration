c6594cbee9d708b0b070f9e4347f0c38b28a7ffb
Brandon Stillwell <brstillwell@csumb.edu>
2018-08-24 05:59:04 +0000

    checking to see if session was readded

--- server/server.js
+++ server/server.js
@@ -3,17 +3,12 @@ var boot = require('loopback-boot');
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var app = module.exports = loopback();
-// var session = require('client-sessions');)
-
+// var session = require('client-sessions');
+var session = require('express-session');
 var path = require('path');
 var crypto = require("crypto");
 var clientCertificateAuth = require('client-certificate-auth');
 var fs = require('fs');
-
-
-//"postgres://postgres:anthony@localhost:5432/mip"; // Your Database Connection
-
-  
 
 app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, '../views'));
@@ -32,6 +27,30 @@ app.use(loopback.static(__dirname+'../views'));
 //   activeDuration: 10 * 60 * 1000,
 // }));
 app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: crypto.randomBytes(20).toString('hex'),
+  resave: false,
+  duration: 60 * 60 * 1000,
+  activeDuration: 10 * 60 * 1000,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+
+// var opts = {
+//   // Server SSL private key and certificate
+//   key: fs.readFileSync('../certs/mojavedata.gov.pem'),
+//   cert: fs.readFileSync('../certs/mojavedata.gov.cer'),
+//   // issuer/CA certificate against which the client certificate will be
+//   // validated. A certificate that is not signed by a provided CA will be
+//   // rejected at the protocol layer.
+//   ca: fs.readFileSync('../certs/ca-bundle.crt'),
+//   // request a certificate, but don't necessarily reject connections from
+//   // clients providing an untrusted or no certificate. This lets us protect only
+//   // certain routes, or send a helpful error message to unauthenticated clients.
+//   requestCert: true,
+//   rejectUnauthorized: false
+// };
 
 // // add clientCertificateAuth to the middleware stack, passing it a callback
 // // which will do further examination of the provided certificate.
@@ -42,6 +61,20 @@ app.set('trust proxy', 1) // trust first proxy
 //   res.send('Authorized!');
 // });
 
+// var checkAuth = function(cert) {
+// /*
+//   * allow access if certificate subject Common Name is 'Doug Prishpreed'.
+//   * this is one of many ways you can authorize only certain authenticated
+//   * certificate-holders; you might instead choose to check the certificate
+//   * fingerprint, or apply some sort of role-based security based on e.g. the OU
+//   * field of the certificate. You can also link into another layer of
+//   * auth or session middleware here; for instance, you might pass the subject CN
+//   * as a username to log the user in to your underlying authentication/session
+//   * management layer.
+//   */
+//   console.log(cert)
+//   return cert.subject.CN === 'Doug Prishpreed';
+// };
 
 
 app.start = function() {
